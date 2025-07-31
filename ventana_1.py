8b6f60923eb81feefb8e25b4751093af5f5ccfc1
sofia479 <bertagalvis@colegioguanenta.edu.co>
2025-07-31 15:57:19 -0500

    Se agrego pantalla

---
+++ ventana_1.py
@@ -0,0 +1,24 @@
+# se importa una libreria tkinder con todas sus funciones
+from tkinter import *
+
+#------------------------
+# funciones de la app
+#----------------------
+
+
+# ------------------------
+# ventana principal de la app
+# ---------------------------
+
+# se declara una variable llamada ventana_pricipal, que adquiere las carateristicas un objeto de tipo tk()
+
+ventana_pricipal = Tk()
+
+# titulo de la ventana
+ventana_pricipal.title("sofiagalviscuellar")
+
+# tamaño de la ventana
+ventana_pricipal.geometry("800x500")
+
+# metodo principal que despliega la ventana en pantalla
+ventana_pricipal.mainloop()
